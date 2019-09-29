072468b771f33d2e1a9b210d8e772e9a2d5ba969
xmeng18 <mfinemxy@hotmail.com>
2019-09-28 20:35:45 -0400

    Sep28_Meng_New_Notes

--- functions.py
+++ functions.py
@@ -37,19 +37,29 @@ def sigmoid(x):
 		print("division by zero!")
 	except Exception as err:
 		print("Error in sigmoid: " + err)
+
+'''
+Why does the Sigmoid function overflow?
+As the training progress, the network improves its precision. As this precision approaches perfection, 
+the sigmoid signal will either approach 1 from below or 0 for above. Eg: either 0.99999999999... or 0.00000000000000001...
+This if condition here does not change the output of sigmoid function, it avoids the possible overflow error when computing the
+sigmoid function
+'''
 	
 
 # returns an an n-day state representation ending at time t
 def getState(data, t, n):
 	Vol, Volume_ratio =  getOtherDataVec()
-
+	#This part gets the data from csv file with OHLC data. If there is no n
+	#periods before time t, the first row of data will be replicated to keep the
+	#number of states to be n
 	d = t - n + 1
 	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
-
+    #We do the similar thing the other data, which includes volume_ratio and Volumn
 	otherdata = np.array([Volume_ratio,Vol])
 
 	new_block = otherdata[:,d:t] if d >= 0 else np.concatenate((np.repeat(np.array([otherdata[:,0]]),-d-1, axis=0).T, otherdata[:,0:t + 1]), 1)
-
+	#We do the sigmoid transformation to the difference of the data of the first part.
 	res = []
 	for i in range(n - 1):
 		res.append(sigmoid(block[i + 1] - block[i]))
